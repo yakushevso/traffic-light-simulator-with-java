456acc81f24ccd228293f4c59da9a0be4f1c3606
Work <yakushevso@ya.ru>
2024-04-09 13:16:37 +0300

    Stage 3/6: Oops, wrong button

--- Traffic Light Simulator with Java/task/src/traffic/UI.java
+++ Traffic Light Simulator with Java/task/src/traffic/UI.java
@@ -1,33 +1,65 @@
 package traffic;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class UI {
+    private final Scanner sc = new Scanner(System.in);
+
     public void start() {
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.println(Messages.WELCOME);
+        System.out.println(Messages.WELCOME);
 
-            System.out.println(Messages.INPUT_ROADS);
-            int roads = sc.nextInt();
+        System.out.println(Messages.INPUT_ROADS);
+        int roads = getPosNum();
 
-            System.out.println(Messages.INPUT_INTERVAL);
-            int interval = sc.nextInt();
+        System.out.println(Messages.INPUT_INTERVAL);
+        int interval = getPosNum();
 
-            while (true) {
-                System.out.println(Messages.MENU);
-                int mode = sc.nextInt();
+        while (true) {
+            clearConsole();
 
-                switch (mode) {
-                    case 0 -> {
-                        System.out.println("Bye!");
-                        return;
-                    }
-                    case 1 -> System.out.println("Road added");
-                    case 2 -> System.out.println("Road deleted");
-                    case 3 -> System.out.println("System opened");
-                    default -> System.out.println("Invalid command!");
+            System.out.println(Messages.MENU);
+            String mode = sc.nextLine();
+
+            switch (mode) {
+                case "0" -> {
+                    System.out.println("Bye!");
+                    return;
                 }
+                case "1" -> System.out.println("Road added");
+                case "2" -> System.out.println("Road deleted");
+                case "3" -> System.out.println("System opened");
+                default -> System.out.println("Incorrect option");
             }
+
+            sc.nextLine();
+        }
+    }
+
+    private int getPosNum() {
+        while (true) {
+            if (sc.hasNextInt()) {
+                int number = sc.nextInt();
+                sc.nextLine();
+
+                if (number > 0) {
+                    return number;
+                }
+            } else {
+                sc.nextLine();
+            }
+
+            System.out.println("Error! Incorrect input. Try again:");
+        }
+    }
+
+    private void clearConsole() {
+        try {
+            var clearCommand = System.getProperty("os.name").contains("Windows")
+                    ? new ProcessBuilder("cmd", "/c", "cls")
+                    : new ProcessBuilder("clear");
+            clearCommand.inheritIO().start().waitFor();
+        } catch (InterruptedException | IOException ignored) {
         }
     }
 }
